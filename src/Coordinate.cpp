7543cf2f78e762114294acee0c154048e6a0a864
Tom de Pasquale <tom.de_pasquale.etu@univ-lemans.fr>
2021-12-06 11:22:50 +0100

    Adding a coordinate system

---
+++ src/Coordinate.cpp
@@ -0,0 +1,31 @@
+//
+// Created by Tom on 06/12/2021.
+//
+
+#include "Coordinate.h"
+
+/**
+ * @brief Depending on the side, this will return the correct position
+ * We keep the origin at the same position (bottom left hand corner of the table) and we translate the x position
+ * depending of the side.
+ * The positions are mirrored on each side.
+ */
+int Coordinate::getAbsoluteX() {
+    if(side == YELLOW) {
+        return x;
+    } else {
+        return TABLE_WIDTH - x;
+    }
+}
+
+int Coordinate::getAbsoluteY() {
+    return y;
+}
+
+int Coordinate::initialize(int currentSide) {
+    if(currentSide == 0) {
+        Coordinate::side = YELLOW;
+    } else {
+        Coordinate::side = PURPLE;
+    }
+}
---
+++ src/Coordinate.h
@@ -0,0 +1,46 @@
+//
+// Created by Tom on 06/12/2021.
+//
+
+#ifndef KRABBS_COORDINATE_H
+#define KRABBS_COORDINATE_H
+
+#define YELLOW 0
+#define PURPLE 1
+
+// We use millimeter as base unit
+// 1 millimeter = 1 unit
+#define TABLE_WIDTH 3000
+#define TABLE_HEIGHT 2000
+
+/**
+ * The coordinates are stored with default yellow side.
+ * Given that the table is symmetrical, we only need instructions for the yellow side and it will automatically convert
+ * them to the correct absolute position on the purple side.
+ */
+
+class Coordinate {
+
+private:
+    static side = YELLOW;
+public:
+    // ***** VARIABLES *****
+    int x, y;
+
+    Coordinate(int x, int y) : x(x), y(y) {
+
+    }
+
+    // ***** METHODS *****
+    /// @brief Initialize on which side the robot is. It is yellow by default
+    static initialize(int currentSide);
+
+    // ----- Getters & Setters -----
+    /// @brief Returns the absolute x position in millimeters
+    getAbsoluteX();
+
+    /// @brief Returns the absolute y position in millimeters
+    getAbsoluteY();
+};
+
+#endif //KRABBS_COORDINATE_H
