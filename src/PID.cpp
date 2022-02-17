654113f625b06d09aafa289c00c54ab2e719d235
Tom de Pasquale <tomarthus72@gmail.com>
2022-02-17 16:02:44 +0100

    First modifications
    Angle is (almost) working

--- src/PID.cpp
+++ src/PID.cpp
@@ -1,25 +1,88 @@
-//
-// Created by Tom on 14/02/2022.
-//
-
 #include "PID.h"
+using namespace std;
+/**
+ * Construction PID
+ * @param kp
+ * @param ki
+ * @param kd
+ * @param min
+ * @param max
+ */
+PID::PID(float kp, float ki, float kd, float min, float max) {
 
-PID::PID(double kP, double kI, double kD) {
-    this->kP = kP;
-    this->kI = kI;
-    this->kD = kD;
+    this->m_kp = kp;
+    this->m_ki = ki;
+    this->m_kd = kd;
+    this->m_min = min;
+    this->m_max = max;
+}
+/**
+ * @brief Calculer le PID
+ *
+ * @param currentState L'état actuelle
+ * @param consigne  Goal
+ * @param timestep
+ * @return
+ */
+float PID::compute(float currentState, float consigne) {
+
+    // Ecart entre la consigne et la mesure
+    float error = consigne - currentState;
+
+    // Proportional term
+    float Pout = m_kp * error;
+
+    //cout << " ERROR : " << error << endl;
+    //cout << " ERROR Pout (kp*error): " << Pout << endl;
+
+    // Integral term
+    this->m_integral += error;
+    float Iout = m_ki * this->m_integral;
+
+    // Derivative term
+    this->m_derivative = error - m_pre_error;
+    float Dout = m_kd * this->m_derivative;
+
+    // Compute the PID controller's output
+    float output = Pout + Iout + Dout;
+
+    // Restrict to max/min
+//    if( output > m_max )
+//        output = m_max;
+//    else if( output < m_min )
+//        output = m_min;
+
+    // Save error to previous error
+    this->m_pre_error = error;
+
+    // Returns the manipulated variable given a setpoint and current process value
+    return output;
 }
 
-double PID::compute(double error) {
-    sumErrors += error;
+void PID::setTunings(float kp, float ki, float kd) {
+    if (kp < 0 || ki < 0 || kd < 0)
+        return;
 
-    double P = error * kP;
-    double I = sumErrors * kI;
-    double D = (error - lastError) * kD;
-
-    lastError = error;
-
-    return P + I + D;
+    this->m_kp = kp;
+    this->m_ki = ki;
+    this->m_kd = kd;
 }
 
+void PID::resetErrors() {
+    this->m_pre_error = 0;
+    this->m_integral = 0;
+    this->m_derivative = 0;
+}
+
+float PID::getCurrentGoal() const {
+    return m_goal;
+}
+
+void PID::setGoal(float goal) {
+    this->m_goal = goal;
+}
+
+double PID::getDeltaError() {
+    return m_derivative;
+}
 
