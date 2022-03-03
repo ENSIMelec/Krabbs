82881a002eefd0cfb8cd64ddfeeddb5a733d071d
Tom de Pasquale <tomarthus72@gmail.com>
2022-03-03 11:55:16 +0100

    Create encoders.ino

---
+++ arduino/encoders/encoders.ino
@@ -0,0 +1,80 @@
+/*
+ * This program is used to get the tick number for each encoder wheel for Krabbs
+ * see https://github.com/ENSIMelec/Krabbs
+ * 
+ * It uses the Serial Port to communicate with the RapsberryPi. 
+ * 
+ * TODO : Depending on the speed limit, it could be nice to use the I2C bus to communicate.
+ * 
+ * Author : Tom de Pasquale
+ * 
+ * Last updated : March 03 2022
+ * Reformating the code to match with recomandations and adding comments.
+ *
+ */
+
+// Uncomment to activate debug mod
+//#define DEBUG
+
+// Interrupt pins
+#define interLeftEncoder    0 //pin2 : interrupt 0 
+#define interRightEncoder   1 //pin3 : interrupt 1 
+
+#define LEFT 0
+#define RIGHT 1
+
+uint8_t tickValues[2];
+
+void setup()
+{
+#ifdef DEBUG
+  Serial.begin(9600);
+#endif
+
+  // Set the tick values to be 0 at start
+  reset();
+
+  // Attach interrupt functions for each encoding wheel
+  attachInterrupt(interRightEncoder, countRightTicks, RISING);
+  attachInterrupt(interLeftEncoder, countLeftTicks, RISING);
+}
+
+/* Reset the tick values */
+void reset()
+{
+  tickValues[LEFT] = 0;
+  tickValues[RIGHT] = 0;
+}
+
+/* We are not using the for loop execpt for debug uses. The code only use interruptions. */
+void loop()
+{
+
+#ifdef DEBUG
+  Serial.print("Left ticks : ");
+  Serial.print(tickValues[LEFT]);
+  Serial.print("\tRight ticks : ");
+  Serial.println(tickValues[RIGHT]);
+#endif
+
+}
+
+/* Increments or decrements the right tick value */
+void countRightTicks ()
+{
+  //if (digitalRead(CANAL_B_RIGHT) == HIGH)
+  if (PINB & 0x01)
+    tickValues[RIGHT]--;
+  else
+    tickValues[RIGHT]++;
+}
+
+/* Increments or decrements the right tick value */
+void countLeftTicks ()
+{
+  //if (digitalRead(CANAL_B_LEFT) == HIGH)
+  if (PIND & 0x80)
+    tickValues[LEFT]--;
+  else
+    tickValues[LEFT]++;
+}
